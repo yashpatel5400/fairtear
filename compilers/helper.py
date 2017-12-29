b3b99faba383e267cf84379a49af43d8916172aa
Yash Patel <ypatel5400@gmail.com>
2017-12-29 16:17:36 -0500

    Refactored simple compiler

--- compilers/helper.py
+++ compilers/helper.py
@@ -15,8 +15,7 @@ import math
 
 import compilers.constants as c
 
-def plot_fit(dataset, data, mu, std):
-    # Plot the PDF.
+def _plot_fit(dataset, data, mu, std):
     plt.hist(data, bins=25, normed=True, alpha=0.6, color='g')
 
     xmin, xmax = plt.xlim()
@@ -29,7 +28,7 @@ def plot_fit(dataset, data, mu, std):
     plt.savefig("output/{}_fit.png".format(dataset))
     plt.close()
 
-def entropy(data, to_plot=True):
+def _entropy(data, to_plot=True):
     num_partitions = 10
 
     arr_range = np.max(data) - np.min(data)
@@ -46,23 +45,23 @@ def entropy(data, to_plot=True):
     entropy = scipy.stats.entropy(probabilities)
     return entropy
 
-def gaussian_mse(data, mu, std):
+def _gaussian_mse(data, mu, std):
     counts, partitions = np.histogram(data, 25)
     dist = scipy.stats.norm(mu, std)
     predicted_counts = np.array([dist.pdf(np.mean([partitions[i],partitions[i+1]])) * len(data) 
         for i in range(len(partitions)-1)])
     return np.square(counts - predicted_counts).mean()
 
-def run_partition(df, partition_column, column, partition):
-    data = df[column]
-    orig_entropy = entropy(data)
-    left_partition  = data[df[partition_column] <= partition]
+def _partition(data, partition_data, partition):
+    orig_entropy = _entropy(data)
+    left_partition  = data[partition_data <= partition]
+    right_partition = data[partition_data > partition]
+    
     left_frac = len(left_partition) / len(data)
-    right_partition = data[df[partition_column] > partition]
     right_frac = len(right_partition) / len(data)
     
-    new_entropy = left_frac * entropy(left_partition) + \
-        right_frac * entropy(right_partition)
+    new_entropy = left_frac * _entropy(left_partition) + \
+        right_frac * _entropy(right_partition)
     information_gain = orig_entropy - new_entropy
     
     mus, stds, mses = [], [], []
@@ -70,8 +69,8 @@ def run_partition(df, partition_column, column, partition):
         ["original", "left", "right"]):
         
         mu, std = scipy.stats.norm.fit(values)
-        plot_fit(dataset, values, mu, std)
-        mse = gaussian_mse(values, mu, std)
+        _plot_fit(dataset, values, mu, std)
+        mse = _gaussian_mse(values, mu, std)
 
         mus.append(mu)
         stds.append(std)
@@ -88,3 +87,24 @@ def run_partition(df, partition_column, column, partition):
     if information_gain < c.INFORMATION_GAIN_THRESH or orig_mse < new_mse:
         return (orig_mu, orig_std), None
     return ((left_mu, left_std), (right_mu, right_std)), new_mse
+
+def make_partition(data, partition_data, num_partitions=5):
+    partition_mean = np.mean(partition_data)
+    partition_std  = np.std(partition_data)
+    partitions = np.linspace(
+        partition_mean - partition_std,
+        partition_mean + partition_std,
+        num_partitions)
+
+    min_mse  = None
+    best_fit = None
+    best_partition = None
+
+    for partition in partitions:
+        fit, mse = _partition(data, partition_data, partition)
+        if mse is not None:
+            if min_mse is None or mse < min_mse:
+                min_mse = mse
+                best_fit = fit
+                best_partition = partition
+    return best_fit, best_partition
