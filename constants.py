faef9fd1107f437f046aa081af2a3f36ff901c2c
Yash Patel <ypatel@princeton.edu>
2017-12-29 12:49:43 -0500

    Finished tree construction code (still needs testing)

---
+++ constants.py
@@ -0,0 +1,8 @@
+"""
+__author__ = Yash Patel and Zachary Liu
+__name__   = construct.py
+__description__ = Module-level constants
+"""
+
+# empirically determined value of where to stop recursion/splitting of the tree
+INFORMATION_GAIN_THRESH = 0.125
