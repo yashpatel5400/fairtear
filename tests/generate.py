ab7deacfad1969883799abbc21d5c4b36545a7f8
Yash Patel <ypatel5400@gmail.com>
2017-12-30 13:27:08 -0500

    Added to generation script

--- tests/generate.py
+++ tests/generate.py
@@ -8,10 +8,54 @@ generator. Generated files follow the decision-tree-like structure of FairSquare
 import pandas as pd
 import numpy as np
 
-def generate(fr, fn):
-    num_samples = 500
-    columns = ["sex", "capital_gain", "age", "education_num"]
+def generate_simple(num_samples=500):
+    """ethnicity = gaussian(0,100)
+    colRank = gaussian(25,100)
+    yExp = gaussian(10,25)
+    if ethnicity > 10:
+        colRank = colRank + 5
+    sensitiveAttribute(ethnicity > 10)
+    """
+    print("Generating simple dataset...")
+    columns = ["ethnicity", "colRank", "yExp", "expRank", "hire"]
+    data = pd.DataFrame()
+    for column in columns:
+        data[column] = np.zeros(num_samples)
 
+    data["ethnicity"] = np.random.normal(loc=0.0, scale=100.0, size=(num_samples))
+    data["colRank"]   = np.random.normal(loc=25.0, scale=100.0, size=(num_samples))
+    data["yExp"]      = np.random.normal(loc=10.0, scale=25.0, size=(num_samples))
+    data["colRank"][data["ethnicity"] > 10] += 5
+
+    data["expRank"] = data["yExp"] - data["colRank"]
+    data["hire"][data["colRank"] <= 5] += 1
+    data["hire"][data["expRank"] > -5] += 1
+    data = data.drop(["expRank"], axis=1)
+
+    data.to_csv("tests/simple.csv", index=False)
+
+def generate_multi(num_samples=500):
+    """sex = gaussian(1,1)
+    if sex < 1:
+        capital_gain = gaussian(568.4105, 24248365.5428)
+        if capital_gain < 7298.0000:
+            age = gaussian(38.4208, 184.9151)
+            education_num = gaussian(10.0827, 6.5096)
+        else:
+            age = gaussian(38.8125, 193.4918)
+            education_num = gaussian(10.1041, 6.1522)
+    else:
+        capital_gain = gaussian(1329.3700, 69327473.1006)
+        if capital_gain < 5178.0000:
+            age = gaussian(38.6361, 187.2435)
+            education_num = gaussian(10.0817, 6.4841)
+        else:
+            age = gaussian(38.2668, 187.2747)
+            education_num = gaussian(10.0974, 7.1793)
+    sensitiveAttribute(sex < 1)
+    """
+    print("Generating multi-level dataset...")
+    columns = ["sex", "capital_gain", "age", "education_num"]
     data = pd.DataFrame()
     for column in columns:
         data[column] = np.zeros(num_samples)
@@ -42,28 +86,12 @@ def generate(fr, fn):
     data["education_num"][(data["sex"] >= 1) & (data["capital_gain"] >= 5178.0000)] = \
         np.random.normal(loc=10.0974, scale=7.1793, size=(num_samples))
     
-    data.to_csv("tests/{}.csv".format(fn), index=False)
+    data.to_csv("tests/multi.csv", index=False)
+
+def generate():
+    num_samples = 500
+    generate_simple(num_samples)
+    generate_multi(num_samples)
 
 if __name__ == "__main__":
-    fr = """
-    sex = gaussian(1,1)
-    if sex < 1:
-        capital_gain = gaussian(568.4105, 24248365.5428)
-        if capital_gain < 7298.0000:
-            age = gaussian(38.4208, 184.9151)
-            education_num = gaussian(10.0827, 6.5096)
-        else:
-            age = gaussian(38.8125, 193.4918)
-            education_num = gaussian(10.1041, 6.1522)
-    else:
-        capital_gain = gaussian(1329.3700, 69327473.1006)
-        if capital_gain < 5178.0000:
-            age = gaussian(38.6361, 187.2435)
-            education_num = gaussian(10.0817, 6.4841)
-        else:
-            age = gaussian(38.2668, 187.2747)
-            education_num = gaussian(10.0974, 7.1793)
-    sensitiveAttribute(sex < 1)
-    """
-
-    generate(fr, "ex")
+    generate()
