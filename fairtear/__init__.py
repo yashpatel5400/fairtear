c12b773c4fb5f0186d5777515913ce1f2bc576cd
Yash Patel <ypatel@princeton.edu>
2018-01-04 11:47:47 -0500

    Fixed typos

--- fairtear/__init__.py
+++ fairtear/__init__.py
@@ -3,7 +3,3 @@ __author__ = Yash Patel and Zachary Liu
 __name__   = app.py
 __description__ = Initializes the Flask app server
 """
-from flask import Flask
-
-app = Flask(__name__)
-app.config.from_object('config')
--- run.py
+++ run.py
@@ -4,5 +4,7 @@ __name__   = run.py
 __description__ = Main file to run to start Flask server
 """
 
-from app import app
-app.run(host='0.0.0.0', port=8080, debug=True)
+from fairtear.app import app
+
+if __name__ == "__main__":
+    app.run(port=8080, debug=True)
