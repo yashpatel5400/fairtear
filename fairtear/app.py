36188068fcb2fedb6192ee603bbb587b54a15007
Yash Patel <ypatel5400@gmail.com>
2018-01-04 19:23:56 -0500

    Added async form submit

--- fairtear/app.py
+++ fairtear/app.py
@@ -8,6 +8,8 @@ import os
 from flask import Flask, request, redirect, url_for, render_template, jsonify
 from werkzeug.utils import secure_filename
 
+from fairtear.forms import DataForm
+
 app = Flask(__name__)
 app.config.from_object("config")
 
@@ -15,17 +17,33 @@ def allowed_file(filename):
     return "." in filename and \
            filename.rsplit(".", 1)[1].lower() in app.config["ALLOWED_EXTENSIONS"]
 
-@app.route('/_analyze_data', methods=["GET", "POST"])
+@app.route('/_analyze_data', methods=["POST"])
 def analyze_data():
-    print(request['xcsv'])
-    xcsv = request.args.get('xcsv', 0)
-    ycsv = request.args.get('ycsv', 0)
-    clf  = request.args.get('clf', 0)
+    form = DataForm(request.form)
+    if request.method == "POST" and form.validate():
+        # check if the post request has the file part
+        filenames = ["xcsv","ycsv","clf"]
+        files = []
 
-    return jsonify(result=a + b)
+        for filename in filenames:
+            if filename not in request.files:
+                return redirect(request.url)
+
+        files = [request.files[filename] for filename in filenames]
+        
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        for file in files:
+            if not file or file.filename == "" or not allowed_file(file.filename):
+                return redirect(request.url)
+
+        for file in files:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
 
 @app.route("/", methods=["GET", "POST"])
 def upload_file():
+    form = DataForm(request.form)
     if request.method == "POST":
         # check if the post request has the file part
         filenames = ["xcsv","ycsv","clf"]
@@ -47,8 +65,4 @@ def upload_file():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
 
-    return render_template("index.html")
-
-@app.route("/test")
-def test():
-    return render_template("test.html")
+    return render_template("index.html", form=form)
