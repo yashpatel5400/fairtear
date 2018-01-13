61fcb6b078c40bc3dec35f8a89a972cde065f08c
Zachary Liu <zachary@zacharyliu.com>
2018-01-13 12:55:55 +0900

    State management

--- fairtear/static/js/scripts.js
+++ fairtear/static/js/scripts.js
@@ -52,39 +52,72 @@ function reducer(state, action) {
 
 var store = Redux.createStore(reducer, initialState);
 
-store.subscribe(() => {
-    console.log(store.getState());
-    
-});
+var currentState = {};
+var attributesPresent = state => state.attributes && state.attributes.length > 0;
+var visibilityMap = {
+    'js-classifier-attributes-help': state => !attributesPresent(state),
+    'js-classifier-attributes-select': state => attributesPresent(state),
+    'js-model-attributes-help': state => !attributesPresent(state),
+    'js-model-attributes-select': state => attributesPresent(state),
+    'js-qualified-select': state => state.qualifiedEnabled,
+};
 
-$(function() {
-  $('a#analyze').bind('click', function() {
-    var form_data = new FormData($('#data')[0]);
-    $.ajax({
-        type: 'POST',
-        url: $SCRIPT_ROOT + '/_analyze_data',
-        data: form_data,
-        contentType: false,
-        cache: false,
-        processData: false,
-        success: function(data) {
-            $("#result").text(data.result);
+function render(nextState) {
+    console.log(currentState, nextState);
+
+    // Show/hide elements
+    for (var className of Object.keys(visibilityMap)) {
+        if (visibilityMap[className](nextState)) {
+            $(`.${className}`).show();
+        } else {
+            $(`.${className}`).hide();
         }
-    });
-    return false;
-  });
+    }
 
-  $('#xcsv').change(function (e) {
-    if (this.files.length == 0) return;
-    var file = this.files[0];
-    Papa.parse(file, {
-        complete: function ({ data }) {
-            store.dispatch({ data, type: 'LOADED_XCSV' });
-        },
-    });
-  });
+    // Update select elements
+    if (nextState.attributes && nextState.attributes.length > 0 && nextState.attributes !== currentState.attributes) {
+        $('.js-attribute-input').each(function () {
+            var defaultItem = $('<option selected disabled>Choose attribute...</option>');
+            var attributeItems = nextState.attributes.map(a => $(`<option value="${a}">${a}</option>`));
+            $(this).empty().append(defaultItem).append(attributeItems);
+        });
+    }
 
-  $('.js-enable-qualified').change(function (e) {
-    store.dispatch({ type: 'SET_QUALIFIED', enabled: this.checked });
-  });
+    currentState = nextState;
+}
+
+store.subscribe(() => render(store.getState()));
+render(initialState);
+
+$(function () {
+    $('#data').submit(function (e) {
+        e.preventDefault();
+        var form_data = new FormData(this);
+        $.ajax({
+            type: 'POST',
+            url: $SCRIPT_ROOT + '/_analyze_data',
+            data: form_data,
+            contentType: false,
+            cache: false,
+            processData: false,
+            success: function (data) {
+                $("#result").text(data.result);
+            }
+        });
+        return false;
+    });
+
+    $('#xcsv').change(function (e) {
+        if (this.files.length == 0) return;
+        var file = this.files[0];
+        Papa.parse(file, {
+            complete: function ({ data }) {
+                store.dispatch({ data, type: 'LOADED_XCSV' });
+            },
+        });
+    });
+
+    $('.js-enable-qualified').change(function (e) {
+        store.dispatch({ type: 'SET_QUALIFIED', enabled: this.checked });
+    });
 });
