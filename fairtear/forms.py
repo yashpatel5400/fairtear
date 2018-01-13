aee855179895d6e1c910c484ed0599d7cd40702e
Zachary Liu <zachary@zacharyliu.com>
2018-01-13 09:09:39 -0500

    Frontend tweaks

--- fairtear/forms.py
+++ fairtear/forms.py
@@ -8,25 +8,22 @@ from flask_wtf import Form, FlaskForm
 
 from flask_wtf.file import FileField, FileRequired
 from wtforms.validators import InputRequired
-from wtforms import StringField, SelectField, FloatField, FileField, FormField
+from wtforms import StringField, SelectField, FloatField, FileField, FormField, BooleanField
 
 def ConditionalField(label, required=True):
     validators = [InputRequired()] if required else []
     return SelectField(label, choices=[(">", ">"), ("=", "="), ("<", "<")], validators=validators)
 
-def AttributeFormClass(required=True):
-    validators = [InputRequired()] if required else []
-    class AttributeForm(Form):
-        attribute   = StringField("Attribute", validators=validators)
-        conditional = SelectField("Conditional", choices=[(">", ">"), ("=", "="), ("<", "<")], validators=validators)
-        threshold   = FloatField("Threshold", validators=validators)
-    return AttributeForm
+class AttributeForm(Form):
+    attribute   = StringField("Attribute", validators=[InputRequired()])
+    conditional = SelectField("Conditional", choices=[(">", ">"), ("=", "="), ("<", "<")], validators=[InputRequired()])
+    threshold   = FloatField("Threshold", validators=[InputRequired()])
 
 class DataForm(FlaskForm):
-    xcsv  = FileField("Features CSV")
-    ycsv  = FileField("Target CSV")
-    clf   = FileField("Classifier Pickle")
+    xcsv  = FileField("Data CSV", validators=[InputRequired()])
+    clf   = FileField("Classifier Pickle", validators=[InputRequired()])
 
-    sensitive = FormField(AttributeFormClass())
-    qualified = FormField(AttributeFormClass(required=False))
-    target = FormField(AttributeFormClass())
+    sensitive = FormField(AttributeForm)
+    enable_qualified = BooleanField()
+    qualified = FormField(AttributeForm)
+    target = FormField(AttributeForm)
